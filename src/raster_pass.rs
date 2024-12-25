351e844e10239f4439ecdd52f4a54a3c33090da7
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2024-12-25 14:42:12 +0000

    reduce number of bindings

--- src/raster_pass.rs
+++ src/raster_pass.rs
@@ -4,42 +4,38 @@ pub struct RasterPass {
 
 impl RasterPass {
     pub fn new(device: &wgpu::Device) -> Self {
-        // Bind Group Layout for Output Buffer
-        let color_bind_group_layout =
+        // Combine output and depth buffers into one bind group layout
+        let frame_buffer_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Raster: Output Buffer Bind Group Layout"),
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage { read_only: false },
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
+                label: Some("Frame Buffer Bind Group Layout"),
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
                     },
-                    count: None,
-                }],
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                ],
             });
 
-        // Bind Group Layout for Depth Buffer
-        let depth_bind_group_layout =
+        // Screen dimensions bind group layout
+        let screen_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Raster: Depth Buffer Bind Group Layout"),
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage { read_only: false },
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-            });
-
-        // Bind Group Layout for Uniforms
-        let uniform_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Raster: Uniform Bind Group Layout"),
+                label: Some("Screen Bind Group Layout"),
                 entries: &[wgpu::BindGroupLayoutEntry {
                     binding: 0,
                     visibility: wgpu::ShaderStages::COMPUTE,
@@ -52,26 +48,10 @@ impl RasterPass {
                 }],
             });
 
-        // Bind Group Layout for Vertex Buffer
-        let vertex_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Raster: Vertex Buffer Bind Group Layout"),
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage { read_only: true },
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-            });
-
-        // Bind Group Layout for Camera Buffer
+        // Camera bind group layout
         let camera_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Raster: Camera Buffer Bind Group Layout"),
+                label: Some("Camera Bind Group Layout"),
                 entries: &[wgpu::BindGroupLayoutEntry {
                     binding: 0,
                     visibility: wgpu::ShaderStages::COMPUTE,
@@ -84,64 +64,71 @@ impl RasterPass {
                 }],
             });
 
+        // Combine vertex and light buffers into one bind group layout
+        let geometry_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("Geometry Bind Group Layout"),
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                ],
+            });
+
+        // Combine texture and texture info buffers into one bind group layout
         let texture_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Texture Buffer Bind Group Layout"),
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage { read_only: true },
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
+                label: Some("Texture Bind Group Layout"),
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
                     },
-                    count: None,
-                }],
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                ],
             });
 
-        let texture_infos_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Texture Infos Bind Group Layout"),
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage { read_only: true },
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-            });
-
-        // Bind Group Layout for Light Buffer
-        let light_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: Some("Light Buffer Bind Group Layout"),
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Storage { read_only: true },
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-            });
-
-        // Create Pipeline Layout with all Bind Group Layouts
+        // Create Pipeline Layout with consolidated bind group layouts
         let layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Raster Pipeline Layout"),
             bind_group_layouts: &[
-                &color_bind_group_layout,
-                &depth_bind_group_layout,
-                &uniform_bind_group_layout,
-                &vertex_bind_group_layout,
+                &frame_buffer_bind_group_layout,
+                &screen_bind_group_layout,
                 &camera_bind_group_layout,
+                &geometry_bind_group_layout,
                 &texture_bind_group_layout,
-                &texture_infos_bind_group_layout,
-                &light_bind_group_layout,
             ],
             push_constant_ranges: &[],
         });
@@ -164,14 +151,11 @@ impl RasterPass {
 }
 
 pub struct RasterBindings {
-    pub output_buffer: wgpu::BindGroup,
-    pub depth_buffer: wgpu::BindGroup,
-    pub uniform: wgpu::BindGroup,
-    pub vertex_buffer: wgpu::BindGroup,
-    pub texture_buffer: wgpu::BindGroup,
-    pub texture_infos_buffer: wgpu::BindGroup,
-    pub camera_buffer: wgpu::BindGroup,
-    pub light_buffer: wgpu::BindGroup,
+    pub frame_buffer: wgpu::BindGroup,
+    pub screen: wgpu::BindGroup,
+    pub camera: wgpu::BindGroup,
+    pub geometry: wgpu::BindGroup,
+    pub texture: wgpu::BindGroup,
 }
 
 impl RasterBindings {
@@ -183,97 +167,84 @@ impl RasterBindings {
         vertex_buffer: &wgpu::Buffer,
         texture_buffer: &wgpu::Buffer,
         texture_infos_buffer: &wgpu::Buffer,
-        uniform: &wgpu::Buffer,
+        screen_uniform: &wgpu::Buffer,
         camera_buffer: &wgpu::Buffer,
         light_buffer: &wgpu::Buffer,
     ) -> Self {
-        // Bind Group for Color Buffer
-        let output_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Raster: Output Buffer Bind Group"),
+        // Frame buffer bind group (color + depth)
+        let frame_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Frame Buffer Bind Group"),
             layout: &pipeline.get_bind_group_layout(0),
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: output_buffer.as_entire_binding(),
-            }],
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: output_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: depth_buffer.as_entire_binding(),
+                },
+            ],
         });
 
-        // Bind Group for Depth Buffer
-        let depth_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Raster: Depth Buffer Bind Group"),
+        // Screen dimensions bind group
+        let screen = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Screen Bind Group"),
             layout: &pipeline.get_bind_group_layout(1),
             entries: &[wgpu::BindGroupEntry {
                 binding: 0,
-                resource: depth_buffer.as_entire_binding(),
+                resource: screen_uniform.as_entire_binding(),
             }],
         });
 
-        // Bind Group for Uniforms
-        let uniform = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Raster: Uniform Bind Group"),
+        // Camera bind group
+        let camera = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Camera Bind Group"),
             layout: &pipeline.get_bind_group_layout(2),
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: uniform.as_entire_binding(),
-            }],
-        });
-
-        // Bind Group for Vertex Buffer
-        let vertex_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Raster: Vertex Buffer Bind Group"),
-            layout: &pipeline.get_bind_group_layout(3),
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: vertex_buffer.as_entire_binding(),
-            }],
-        });
-
-        // Bind Group for Camera Buffer
-        let camera_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Raster: Camera Buffer Bind Group"),
-            layout: &pipeline.get_bind_group_layout(4),
             entries: &[wgpu::BindGroupEntry {
                 binding: 0,
                 resource: camera_buffer.as_entire_binding(),
             }],
         });
 
-        let texture_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Texture Buffer Bind Group"),
-            layout: &pipeline.get_bind_group_layout(5),
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: texture_buffer.as_entire_binding(),
-            }],
+        // Geometry bind group (vertices + lights)
+        let geometry = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Geometry Bind Group"),
+            layout: &pipeline.get_bind_group_layout(3),
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: vertex_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: light_buffer.as_entire_binding(),
+                },
+            ],
         });
 
-        let texture_infos_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Texture Infos Bind Group"),
-            layout: &pipeline.get_bind_group_layout(6),
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: texture_infos_buffer.as_entire_binding(),
-            }],
-        });
-
-        // Bind Group for Light Buffer
-        let light_buffer = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: Some("Light Buffer Bind Group"),
-            layout: &pipeline.get_bind_group_layout(7),
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: light_buffer.as_entire_binding(),
-            }],
+        // Texture bind group (texture data + info)
+        let texture = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Texture Bind Group"),
+            layout: &pipeline.get_bind_group_layout(4),
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: texture_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: texture_infos_buffer.as_entire_binding(),
+                },
+            ],
         });
 
         Self {
-            output_buffer,
-            depth_buffer,
-            uniform,
-            vertex_buffer,
-            texture_buffer,
-            texture_infos_buffer,
-            camera_buffer,
-            light_buffer,
+            frame_buffer,
+            screen,
+            camera,
+            geometry,
+            texture,
         }
     }
 }
@@ -288,14 +259,11 @@ impl<'a> RasterPass {
         'a: 'pass,
     {
         cpass.set_pipeline(&self.pipeline);
-        cpass.set_bind_group(0, &bindings.output_buffer, &[]);
-        cpass.set_bind_group(1, &bindings.depth_buffer, &[]);
-        cpass.set_bind_group(2, &bindings.uniform, &[]);
-        cpass.set_bind_group(3, &bindings.vertex_buffer, &[]);
-        cpass.set_bind_group(4, &bindings.camera_buffer, &[]);
-        cpass.set_bind_group(5, &bindings.texture_buffer, &[]);
-        cpass.set_bind_group(6, &bindings.texture_infos_buffer, &[]);
-        cpass.set_bind_group(7, &bindings.light_buffer, &[]);
+        cpass.set_bind_group(0, &bindings.frame_buffer, &[]);
+        cpass.set_bind_group(1, &bindings.screen, &[]);
+        cpass.set_bind_group(2, &bindings.camera, &[]);
+        cpass.set_bind_group(3, &bindings.geometry, &[]);
+        cpass.set_bind_group(4, &bindings.texture, &[]);
         cpass.dispatch_workgroups(dispatch_size, 1, 1);
     }
 }
