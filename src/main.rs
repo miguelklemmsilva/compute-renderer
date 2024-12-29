4fee3e1520fb0a4da97eb564e4cd184f82221149
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2024-12-29 18:05:48 +0000

    add cross hatching

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,16 @@ fn main() {
     let height = 900;
     let width = 1600;
 
+    let lights = vec![
+        ([0.0, 10.0, 5.0], [1.0, 0.8, 0.0], 3.0), // Yellow top light
+        ([-10.0, 0.0, 5.0], [0.0, 1.0, 1.0], 3.0), // Cyan left light
+        ([10.0, 0.0, 5.0], [1.0, 0.0, 1.0], 3.0), // Magenta right light
+    ];
+
     // List of scenes to benchmark
     let scenes = vec![
         SceneConfig {
-            name: "African head - Wave Effect".to_string(),
+            name: "African head - Crosshatch Effect".to_string(),
             model_path: get_asset_path("african_head.obj")
                 .to_string_lossy()
                 .to_string(),
@@ -31,12 +37,8 @@ fn main() {
                     .to_string_lossy()
                     .to_string(),
             ),
-            lights: vec![
-                ([0.0, 10.0, 5.0], [1.0, 0.2, 0.2], 5.0),  // Red top light
-                ([-10.0, 0.0, 5.0], [0.2, 0.2, 1.0], 3.0), // Blue left light
-                ([10.0, 0.0, 5.0], [0.2, 1.0, 0.2], 3.0),  // Green right light
-            ],
-            effects: Some(vec![Effect::wave_radial(0.3, 4.0, 1.0)]),
+            lights: lights.clone(),
+            effects: Some(vec![Effect::cross_hatch(0.3, 0.001, 1.0)]),
         },
         SceneConfig {
             name: "African head - Dissolve Effect".to_string(),
@@ -48,22 +50,14 @@ fn main() {
                     .to_string_lossy()
                     .to_string(),
             ),
-            lights: vec![
-                ([0.0, 10.0, 5.0], [1.0, 0.8, 0.0], 3.0), // Yellow top light
-                ([-10.0, 0.0, 5.0], [0.0, 1.0, 1.0], 3.0), // Cyan left light
-                ([10.0, 0.0, 5.0], [1.0, 0.0, 1.0], 3.0), // Magenta right light
-            ],
+            lights: lights.clone(),
             effects: Some(vec![Effect::dissolve(20.0, 1.0, 3.0)]),
         },
         SceneConfig {
             name: "Suzanne - Smooth to Flat".to_string(),
             model_path: get_asset_path("suzanne.obj").to_string_lossy().to_string(),
             texture_path: None,
-            lights: vec![
-                ([0.0, 10.0, 5.0], [1.0, 1.0, 1.0], 1.0),  // Red top light
-                ([-10.0, 0.0, 5.0], [1.0, 1.0, 1.0], 0.5), // Blue left light
-                ([10.0, 0.0, 5.0], [1.0, 1.0, 1.0], 0.5),  // Green right light
-            ],
+            lights: lights.clone(),
             effects: Some(vec![Effect::smooth_to_flat(1.0, 4.0)]),
         },
         SceneConfig {
@@ -76,22 +70,14 @@ fn main() {
                     .to_string_lossy()
                     .to_string(),
             ),
-            lights: vec![
-                ([0.0, 10.0, 5.0], [1.0, 0.8, 0.0], 3.0), // Yellow top light
-                ([-10.0, 0.0, 5.0], [0.0, 1.0, 1.0], 3.0), // Cyan left light
-                ([10.0, 0.0, 5.0], [1.0, 0.0, 1.0], 3.0), // Magenta right light
-            ],
+            lights: lights.clone(),
             effects: Some(vec![Effect::pixelate(4.0, 32.0, 2.0)]),
         },
         SceneConfig {
             name: "Suzanne - Voxelize".to_string(),
             model_path: get_asset_path("suzanne.obj").to_string_lossy().to_string(),
             texture_path: None,
-            lights: vec![
-                ([0.0, 10.0, 5.0], [1.0, 0.2, 0.2], 3.0),  // Red top light
-                ([-10.0, 0.0, 5.0], [0.2, 0.2, 1.0], 3.0), // Blue left light
-                ([10.0, 0.0, 5.0], [0.2, 1.0, 0.2], 3.0),  // Green right light
-            ],
+            lights: lights.clone(),
             effects: Some(vec![Effect::voxelize(15.0, 40.0, 1.)]),
         },
     ];
