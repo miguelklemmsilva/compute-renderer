6436c35b093500aa04da27ad0223b91490b40919
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2025-01-09 12:08:24 +0000

    add tile-based rendering

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -1,13 +1,13 @@
 mod clear_pass;
-mod vertex_pass;
-mod raster_pass;
 mod fragment_pass;
 pub mod gpu;
 mod gpu_buffers;
+mod raster_pass;
+mod vertex_pass;
 
-use gpu_buffers::GpuBuffers;
-use clear_pass::ClearPass;
-use vertex_pass::VertexPass;
-use raster_pass::RasterPass;
-use fragment_pass::FragmentPass;
 use crate::util::dispatch_size;
+use clear_pass::ClearPass;
+use fragment_pass::FragmentPass;
+use gpu_buffers::GpuBuffers;
+use raster_pass::RasterPass;
+use vertex_pass::VertexPass;
