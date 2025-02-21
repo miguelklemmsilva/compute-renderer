966d44acd2892988a0bc3a82ecb85bbeee14b09b
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2025-02-21 16:48:37 +0000

    remove performance collector from main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use std::time::Duration;
-
-use performance::PerformanceCollector;
 use scene::{CameraConfig, SceneConfig};
 use window::{BackendType, Window};
 use winit::event_loop::{ControlFlow, EventLoop};
@@ -21,19 +18,21 @@ fn main() {
     let width = 1600;
 
     // List of scenes to benchmark
-    let scenes = vec![SceneConfig {
-        name: "test".to_string(),
-        model_path: String::from("suzanne.obj"),
-        camera_config: CameraConfig::new_first_person(),
-        ..Default::default()
-    },
-    SceneConfig {
-        name: "test".to_string(),
-        model_path: String::from("sibenik/sibenik.obj"),
-        camera_config: CameraConfig::new_first_person(),
-        backend_type: BackendType::WgpuPipeline,
-        ..Default::default()
-    }];
+    let scenes = vec![
+        SceneConfig {
+            name: "test".to_string(),
+            model_path: String::from("sibenik/sibenik.obj"),
+            camera_config: CameraConfig::new_first_person(),
+            backend_type: BackendType::WgpuPipeline,
+            ..Default::default()
+        },
+        SceneConfig {
+            name: "test".to_string(),
+            model_path: String::from("dragon.obj"),
+            camera_config: CameraConfig::new_first_person(),
+            ..Default::default()
+        },
+    ];
 
     // Create a single event loop for all scenes
     let event_loop = EventLoop::new().expect("Failed to create event loop");
@@ -43,23 +42,17 @@ fn main() {
     let current_scene = 0;
     let scene_config = &scenes[current_scene];
 
-    let collector = PerformanceCollector::new(
-        scene_config.name.clone(),
-        current_scene,
-        Duration::from_secs(scene_config.benchmark_duration_secs),
-    );
-
     let scene = pollster::block_on(scene::Scene::from_config(scene_config, width, height));
 
     // Create window with the same backend type as the scene
-    let mut window =
-        match Window::new_with_window(width, height, scene, collector, scene_config.backend_type) {
-            Ok(window) => window,
-            Err(e) => {
-                eprintln!("Failed to create scene {}: {}", scene_config.name, e);
-                return;
-            }
-        };
+    let mut window = match Window::new_with_window(width, height, scene, scene_config.backend_type)
+    {
+        Ok(window) => window,
+        Err(e) => {
+            eprintln!("Failed to create scene {}: {}", scene_config.name, e);
+            return;
+        }
+    };
 
     // Store scenes in window for cycling
     window.set_scene_configs(scenes);
